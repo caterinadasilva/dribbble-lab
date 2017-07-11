eb3eb3d82c806a907f7b8094a1198c54ec1b4bab
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-07-11 10:41:38 -0400

    js donde se ve el objeto en la consola de los proyectos del usuario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-/* Acá va todo el jquery */
+$(function() {
+  $.getJSON('https://api.dribbble.com/v1/users/joshuanoom/shots?access_token=d545bbfb4f69352bf966d099335df4044151eb8d928e6a9cbb87d7f97f299d8e&callback=?', function(resp) {
+    if (resp.data.length > 0) {							
+      $.each(resp.data.reverse(), function(i, val) {
+      	console.log(val);
+   
+      });
+    }
+    else {
+      console.log('No shots');
+    }
+  });		   							    
+});
