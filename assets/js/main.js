ab1d2ee4ee7413cc8a433a34203aac0816a147a8
alefranii <afmormar@gmail.com>
2017-07-11 13:22:20 -0400

    avances caja desde jquery en conjunto con marcela

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,26 @@ $(function() {
     if (resp.data.length > 0) {							
       $.each(resp.data.reverse(), function(i, val) {
       	console.log(val);
-   
+        var proyecto = $("<div>").addClass('listaProyecto');
+        var listaLink = $("<a>").addClass('listaLink').attr("href" , val.html_url);
+        proyecto.append(listaLink);
+        proyecto.addClass("col-lg-4");
+        var imagen = $("<img>").addClass('imagen').attr('src' , val.images.teaser);
+        listaLink.append(imagen);
+
+
+        var info = $("<div>").addClass("info");
+        var h5 = $("<h5>").text(val.title);
+        //var parrafo = $("<p>").text(val.description);
+
+        info.append(h5);
+        info.append(val.description);
+        listaLink.append(info);
+
+        $("#proyectos").append(proyecto);
+
+
+
       });
     }
     else {
