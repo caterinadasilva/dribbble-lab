743c447836cd89e9c53d3b0fc3fe53f71f654a20
Syrimande <caterinadasilva@gmail.com>
2017-07-11 19:59:16 -0400

    estilos y estructura terminada, affix y efecto en scroll

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,3 +1,22 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+$(document).ready(function($) {
+	$(function () {
+	  $('[data-toggle="tooltip"]').tooltip()
+	});
+	sidebar();
+});
+
+function sidebar() {
+		var lastScrollTop = 0;
+		window.addEventListener("scroll", function() {
+			var currentScroll = window.pageYOffset || document.body.scrollTop;
+			if (currentScroll > lastScrollTop) {
+				document.getElementById("links").classList.add("hidden");
+			} else {
+				document.getElementById("links").classList.add("hidden");
+				if ( currentScroll <= 2 ) {
+					document.getElementById("links").classList.remove("hidden");
+				}
+			}
+			lastScrollTop = currentScroll;
+		}, false);
+	}
