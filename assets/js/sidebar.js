122bf9b3739dc544e3ccd0723fbe3eb1eb168ec5
Syrimande <caterinadasilva@gmail.com>
2017-07-11 20:31:33 -0400

    funcionalidad botón en responsive terminada

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -2,21 +2,35 @@ $(document).ready(function($) {
 	$(function () {
 	  $('[data-toggle="tooltip"]').tooltip()
 	});
+
+	$("#btn-hide").click(function(event) {
+		if ($("#links").hasClass('hidden') || $("#skills").hasClass('hidden')) {
+			$("#btn-hide").html('Less <i class="fa fa-chevron-up" aria-hidden="true"></i>')
+			$("#links").removeClass('hidden');
+			$("#skills").removeClass('hidden');
+		} else {
+			$("#btn-hide").html('More <i class="fa fa-chevron-down" aria-hidden="true"></i>')
+			$("#links").addClass('hidden');
+			$("#skills").addClass('hidden');
+		}
+	});
+
 	sidebar();
+
 });
 
 function sidebar() {
-		var lastScrollTop = 0;
-		window.addEventListener("scroll", function() {
-			var currentScroll = window.pageYOffset || document.body.scrollTop;
-			if (currentScroll > lastScrollTop) {
-				document.getElementById("links").classList.add("hidden");
-			} else {
-				document.getElementById("links").classList.add("hidden");
-				if ( currentScroll <= 2 ) {
-					document.getElementById("links").classList.remove("hidden");
-				}
+	var lastScrollTop = 0;
+	window.addEventListener("scroll", function() {
+		var currentScroll = window.pageYOffset || document.body.scrollTop;
+		if (currentScroll > lastScrollTop) {
+			$("#links").addClass(".aside-hide");
+		} else {
+			$("#links").addClass(".aside-hide");
+			if ( currentScroll <= 2 ) {
+				$("#links").removeClass(".aside-hide");
 			}
-			lastScrollTop = currentScroll;
-		}, false);
-	}
+		}
+		lastScrollTop = currentScroll;
+	}, false);
+}
