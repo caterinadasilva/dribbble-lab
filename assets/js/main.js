f9026cbcb0d909d901519e6b1b85789a47a1b4d8
PauLeeR <lindaliel@gmail.com>
2017-07-12 01:27:46 -0400

    menu bottom bien feo, falta mucho

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,12 @@ $(function() {
   $.getJSON('https://api.dribbble.com/v1/users/joshuanoom/shots?access_token=d545bbfb4f69352bf966d099335df4044151eb8d928e6a9cbb87d7f97f299d8e&callback=?', function(resp) {
     if (resp.data.length > 0) {							
       $.each(resp.data.reverse(), function(i, val) {
-      	console.log(val);
+      	//console.log(val);
    
       });
     }
     else {
-      console.log('No shots');
+      //console.log('No shots');
     }
   });		   							    
 });
